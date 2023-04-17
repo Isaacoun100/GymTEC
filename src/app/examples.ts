e5c15f87377e14156110ab8a1e531e7d259fce4a
Isaacoun100 <Isaacoun100@gmail.com>
2023-04-16 18:09:45 -0600

    Examples updated

--- src/app/examples.ts
+++ src/app/examples.ts
@@ -24,7 +24,7 @@ export const branches = [
   }
 
 
-  export const getBranch = {
+  export const get_branch = {
     "status": "ok",
     "result": {
     "nombre_sucursal": "Example Spa",
@@ -44,7 +44,7 @@ export const branches = [
         }
     }
 
-    export const  getAllEquipment = {
+    export const  get_all_equipment = {
         "status": "ok",
         "result": [
             {
@@ -95,10 +95,115 @@ export const branches = [
                 "descripcion": "Mensual"
             }
         ]
+    }
+
+    export const get_all_products = {
+        "status": "ok",
+        "result": [
+            {
+                "nombre_producto": "Camisa Polo",
+                "costo": 29.99
+            },
+            {
+                "nombre_producto": "Jeans",
+                "costo": 59.99
+            },
+            {
+                "nombre_producto": "Zapatos de cuero",
+                "costo": 129.99
+            },
+            {
+                "nombre_producto": "Reloj de pulsera",
+                "costo": 89.99
+            },
+            {
+                "nombre_producto": "Gorra",
+                "costo": 19.99
+            }
+            
+        ]
+    }
+
+    export const get_all_inventories = {
+        "status": "ok",
+        "result": [
+            {
+                "num_serie": 12345,
+                "marca": "Sony",
+                "costo": 399.99
+            },
+            {
+                "num_serie": 67890,
+                "marca": "Samsung",
+                "costo": 549.99
+            },
+            {
+                "num_serie": 24680,
+                "marca": "LG",
+                "costo": 299.99
+            },
+            {
+                "num_serie": 13579,
+                "marca": "Apple",
+                "costo": 749.99
+            },
+            {
+                "num_serie": 86420,
+                "marca": "Lenovo",
+                "costo": 699.99
+            }
+        ]
+    }
+
+    export const get_all_employees = {
+        "status": "ok",
+        "result": [
+            {
+                "cedula": "123456789",
+                "nombre": "Juan",
+                "apellido_1": "Pérez",
+                "apellido_2": "González"
+            },
+            {
+                "cedula": "987654321",
+                "nombre": "María",
+                "apellido_1": "Rodríguez",
+                "apellido_2": "Sánchez"
+            },
+            {
+                "cedula": "456789123",
+                "nombre": "Carlos",
+                "apellido_1": "Gutiérrez",
+                "apellido_2": "Jiménez"
+            },
+            {
+                "cedula": "321654987",
+                "nombre": "Ana",
+                "apellido_1": "López",
+                "apellido_2": "Hernández"
+            },
+            {
+                "cedula": "789123456",
+                "nombre": "Pedro",
+                "apellido_1": "García",
+                "apellido_2": "Martínez"
+            }
+        ]
+    }
+
+    export const adminInfo = {
+        "status":"ok",
+        "result":{
+          "cedula":"123456789",
+          "nombre":"Juan",
+          "apellido_1":"Perez",
+          "apellido_2":"Gonzalez",
+          "provincia":"San Jose",
+          "canton":"Central",
+          "distrito":"Pavas",
+          "salario":3000.00,
+          "correo":"juan.perez@example.com",
+          "password":"p@ssw0rd123"
         }
-        
-        
-        
-        
-        
-        
+      }
+      
