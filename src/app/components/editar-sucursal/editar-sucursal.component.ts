b0460c9d2ac8db00d3135159e2ba822558507f2c
Isaacoun100 <Isaacoun100@gmail.com>
2023-04-23 20:34:00 -0600

    Agregar Sucursal Completo

--- src/app/components/editar-sucursal/editar-sucursal.component.ts
+++ src/app/components/editar-sucursal/editar-sucursal.component.ts
@@ -1,7 +1,7 @@
 import { AddBranch } from './../../models/branch/add-branch';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { get_branch } from 'src/app/examples';
 
@@ -10,28 +10,57 @@ import { get_branch } from 'src/app/examples';
   templateUrl: './editar-sucursal.component.html',
   styleUrls: ['./editar-sucursal.component.scss']
 })
-export class EditarSucursalComponent {
+export class EditarSucursalComponent implements OnInit {
+
+  productIdFromRoute: string | null | undefined ;
 
   get_branch = get_branch;
-
-  constructor(){}
-
-  sucursalForm = new FormControl({
-
-    nombre_sucursal:  ['', Validators.required],
-    fecha_apertura: ['', Validators.required],
-    horario: ['2000-01-01', Validators.required],
-    cap_max: [0, Validators.required],
-    provincia: ['', Validators.required],
-    canton: ['', Validators.required],
-    distrito: ['', Validators.required],
-    manager: ['', Validators.required],
-    telefono: [[]],
-    active_spa: [false],
-    active_store: [false],
-
+  sucursalForm = new FormGroup({
+      
+    nombre_sucursal: new FormControl(get_branch.result.nombre_sucursal, Validators.required) ,
+    fecha_apertura: new FormControl(get_branch.result.fecha_apertura, Validators.required),
+    horario: new FormControl(get_branch.result.horario, Validators.required),
+    cap_max: new FormControl(get_branch.result.cap_max, Validators.required),
+    provincia: new FormControl(get_branch.result.provincia, Validators.required),
+    canton: new FormControl(get_branch.result.canton, Validators.required),
+    distrito: new FormControl(get_branch.result.distrito, Validators.required),
+    manager: new FormControl(get_branch.result.distrito, Validators.required),
+    telefonos: new FormArray([
+      new FormControl(get_branch.result.telefonos[0], Validators.required),
+      new FormControl(get_branch.result.telefonos[1], Validators.required)
+    ]),
+    active_spa: new FormControl(get_branch.result.active_spa, Validators.required),
+    active_store: new FormControl(get_branch.result.active_store, Validators.required),
   });
 
-  editarSucursal(form:AddBranch){}
+  constructor( private route:ActivatedRoute ){}
+  
+  ngOnInit(){
+    const routeParams = this.route.snapshot.paramMap;
+    this.productIdFromRoute = routeParams.get('sucursalNombre');
+  }
+  
+  editarSucursal(form:AddBranch){
+    // Use the string variable productIdFromRoute, it contains the name of the Sucursal were we are in
+    console.log(this.productIdFromRoute);
+  }
+  
+  nuevoTelefono(){
+    const control = <FormArray> this.sucursalForm.controls['telefonos'];
+    control.push(new FormControl('', Validators.required))
+  }
+  
+  eliminarTelefono(i: number){
+    const control = <FormArray> this.sucursalForm.controls['telefonos'];
+    control.removeAt(i)
+  }
+  
+  trackByFn(index: any, item: any) {
+    return index;
+  }
+  
+  eliminarSucursal(){
+    console.log(this.sucursalForm.controls['telefonos'].value);
+  }
 
 }
--- src/app/models/branch/add-branch.ts
+++ src/app/models/branch/add-branch.ts
@@ -1,13 +1,13 @@
 export interface AddBranch {
-  nombre_sucursal: string | undefined;
-  fecha_apertura: string | undefined;
-  horario: string | undefined;
-  cap_max: number | undefined;
-  provincia: string | undefined;
-  canton: string | undefined;
-  distrito: string | undefined;
-  manager: string | undefined;
-  telefonos: [] | undefined;
-  active_spa: boolean | undefined;
-  active_store: boolean | undefined;
+  nombre_sucursal: string | null;
+  fecha_apertura: string | null;
+  horario: string | null;
+  cap_max: number | null;
+  provincia: string | null;
+  canton: string | null;
+  distrito: string | null;
+  manager: string | null;
+  telefonos: (string | null)[];
+  active_spa: boolean | null;
+  active_store: boolean | null;
 }
