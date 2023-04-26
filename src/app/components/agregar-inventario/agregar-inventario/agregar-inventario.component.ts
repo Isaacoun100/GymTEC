0d2a781f131c8855514062984a3a6c96b3f9a78f
Naheem Johnson <naheemjohnson@estudiantec.cr>
2023-04-25 22:58:58 -0600

    FormGroup Inventory finished

--- src/app/components/agregar-inventario/agregar-inventario/agregar-inventario.component.ts
+++ src/app/components/agregar-inventario/agregar-inventario/agregar-inventario.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ProxyInventarioService } from 'src/app/service/inventario/proxy-inventario.service';
 @Component({
   selector: 'app-agregar-inventario',
   templateUrl: './agregar-inventario.component.html',
-  styleUrls: ['./agregar-inventario.component.scss']
+  styleUrls: ['./agregar-inventario.component.scss'],
 })
 export class AgregarInventarioComponent {
-
+  inventarioForm = new FormGroup({
+    num_serie: new FormControl('', Validators.required),
+    marca: new FormControl('', Validators.required),
+    costo: new FormControl('', Validators.required),
+    is_used: new FormControl(false, Validators.required),
+    tipo_equipo: new FormControl('', Validators.required),
+    nombre_sucursal: new FormControl('', Validators.required),
+  });
+  constructor(private proxyInventarioService: ProxyInventarioService) {}
+  ngOnInit(): void {
+    this.proxyInventarioService.currentInventory.subscribe(
+      (inventarioForm) => (this.inventarioForm = inventarioForm)
+    );
+  }
 }
