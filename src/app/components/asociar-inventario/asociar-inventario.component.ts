45edf39adcfea4a59bb63f8cee277e0d750aba24
Isaacoun100 <Isaacoun100@gmail.com>
2023-04-29 02:18:56 -0600

    Asociar inventario view complete

--- src/app/components/asociar-inventario/asociar-inventario.component.ts
+++ src/app/components/asociar-inventario/asociar-inventario.component.ts
@@ -1,21 +1,53 @@
+import { get_all_inventories } from './../../examples';
 import { AssociateInventory } from 'src/app/models/inventory/associate-inventory';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { AddService } from 'src/app/models/services/add-service';
+import { branches } from 'src/app/examples';
 
 @Component({
   selector: 'app-asociar-inventario',
   templateUrl: './asociar-inventario.component.html',
   styleUrls: ['./asociar-inventario.component.scss'],
 })
-export class AsociarInventarioComponent {
+export class AsociarInventarioComponent implements OnInit {
+
+  branches = branches;
+  inventories = get_all_inventories;
+
   asociarIntenvetarioForm = new FormGroup({
     sucursal: new FormControl('', Validators.required),
-    equipo: new FormControl('', Validators.required),
+    num_serie: new FormControl(0, Validators.required),
   });
 
   constructor(private route: ActivatedRoute) {}
 
-  ngonInit(): void {}
+  ngOnInit(): void {}
+
+  asociarInventario(form: any) {
+    // Recordar crear un mensaje de error si el form no pudo ser ingresados
+    console.log(form);
+  }
+
+  /**
+   * This method is used to change the value of the branch name
+   * @param e 
+   */
+  cambiarSucursal(e: any) {
+    this.asociarIntenvetarioForm.controls['sucursal']?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  /**
+   * This method is used to change the value of the service name
+   * @param e 
+   */
+  cambiarNumSerie(e: any) {
+    this.asociarIntenvetarioForm.controls['num_serie']?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
 }
