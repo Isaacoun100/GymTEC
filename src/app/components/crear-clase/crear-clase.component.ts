d5ed5af4fa7c0d5fab59a5cd340064dc1cea93be
Isaacoun100 <Isaacoun100@gmail.com>
2023-04-29 02:20:17 -0600

    Crear clase view complete

--- src/app/components/crear-clase/crear-clase.component.ts
+++ src/app/components/crear-clase/crear-clase.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-clase',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-clase.component.scss']
 })
 export class CrearClaseComponent{
+
+  createClaseForm = new FormGroup({
+    tipo: new FormControl('', Validators.required),
+    modo: new FormControl('', Validators.required),
+    capacidad: new FormControl(0, [Validators.required, Validators.min(1)]),
+    fecha: new FormControl('', Validators.required),
+    hora_ingreso: new FormControl('', Validators.required),
+    hora_salida: new FormControl('', Validators.required),
+    encargado: new FormControl('', Validators.required),
+  });
+
+  createForm(form:any){
+    console.log(form);
+  }
 
 }
