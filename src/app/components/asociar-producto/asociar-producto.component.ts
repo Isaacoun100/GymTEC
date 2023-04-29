cd3aa3582308bf02ae388785a9147acbed8b2e17
Isaacoun100 <Isaacoun100@gmail.com>
2023-04-29 02:19:15 -0600

    Asociar producto view complete

--- src/app/components/asociar-producto/asociar-producto.component.ts
+++ src/app/components/asociar-producto/asociar-producto.component.ts
@@ -1,3 +1,5 @@
+import { branches } from 'src/app/examples';
+import { get_all_products } from './../../examples';
 import { AssociateProduct } from 'src/app/models/product/associate-product';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -10,6 +12,10 @@ import { AddService } from 'src/app/models/services/add-service';
   styleUrls: ['./asociar-producto.component.scss'],
 })
 export class AsociarProductoComponent {
+
+  products = get_all_products;
+  branches = branches;
+
   asociarProductoForm = new FormGroup({
     sucursal: new FormControl('', Validators.required),
     producto: new FormControl('', Validators.required),
@@ -18,4 +24,30 @@ export class AsociarProductoComponent {
   constructor(private route: ActivatedRoute) {}
 
   ngonInit(): void {}
+
+  asociarProducto(form: any) {
+    // Recordar crear un mensaje de error si el form no pudo ser ingresados
+    console.log(form);
+  }
+
+  /**
+   * This method is used to change the value of the branch name
+   * @param e 
+   */
+  cambiarSucursal(e: any) {
+    this.asociarProductoForm.controls['sucursal']?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  /**
+   * This method is used to change the value of the service name
+   * @param e 
+   */
+  cambiarProducto(e: any) {
+    this.asociarProductoForm.controls['producto']?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
 }
