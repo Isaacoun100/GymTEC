0db85562c46f59309f89927864e986cfb331d06f
Naheem Johnson <naheemjohnson@estudiantec.cr>
2023-04-29 18:16:47 -0600

    Added missing payroll service

--- src/app/service/planilla/planilla.service.ts
+++ src/app/service/planilla/planilla.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { ResponseTemplateI } from '../../models/responseTemplate.interface';
+import { GetPayroll } from 'src/app/models/payroll/get-payroll';
+import { get_all_payrolls } from 'src/app/examples';
+
+import * as myGlobals from '../../../../src/app/globals';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlanillaService {
+  url = myGlobals.URL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPayroll(form: GetPayroll): Observable<ResponseTemplateI> {
+    let address = this.url + 'get_payroll';
+
+    return this.http.put<ResponseTemplateI>(address, form);
+  }
+
+  getAllPayrolls(): Observable<ResponseTemplateI> {
+    let address = this.url + 'get_all_payrolls';
+
+    return this.http.get<ResponseTemplateI>(address);
+  }
 }
